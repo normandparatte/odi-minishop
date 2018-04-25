2dab725d15a3783fa22eecbff88b6a94677e5d2b
normandparatte <normand.paratte@gmail.com>
2018-04-25 15:35:56 +0200

    L'API REST est fonctionnel pour ce qui concerne les clients

--- src/main/java/ch/hearc/ig/odi/minishop/business/Customer.java
+++ src/main/java/ch/hearc/ig/odi/minishop/business/Customer.java
@@ -1,14 +1,80 @@
 package ch.hearc.ig.odi.minishop.business;
 
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
 
 public class Customer implements Serializable {
 
-  public Customer() {
+  // -----------------------------------------------------------------------------------------------
+  // ----- PROPRIETES ------------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+  private long customerid;
+  private String firstname;
+  private String lastname;
+  private String username;
+  private String email;
+  private String phone;
 
+  // -----------------------------------------------------------------------------------------------
+  // ----- CONSTRUCTEURS ---------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+  public Customer(int customerid, String firstname, String lastname, String username, String email,
+      String phone) {
+    this.customerid = customerid;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.username = username;
+    this.email = email;
+    this.phone = phone;
   }
 
+  // -----------------------------------------------------------------------------------------------
+  // ----- GETTERS & SETTERS -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+  public long getCustomerid() {
+    return customerid;
+  }
+
+  public void setCustomerid(long customerid) {
+    this.customerid = customerid;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 }
